3624be55dc2a0e910829c40dfbb0b5efa38752d5
Mehmet Kaya <hm.k46@hotmail.com>
2023-01-21 10:10:09 +0300

    deneme

---
+++ DataAccessLayer/Migrations/202301210616173_mig1.cs
@@ -0,0 +1,26 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Contents", "WriterID", "dbo.Writers");
+            DropIndex("dbo.Contents", new[] { "WriterID" });
+            AlterColumn("dbo.Contents", "WriterID", c => c.Int());
+            CreateIndex("dbo.Contents", "WriterID");
+            AddForeignKey("dbo.Contents", "WriterID", "dbo.Writers", "WriterID");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Contents", "WriterID", "dbo.Writers");
+            DropIndex("dbo.Contents", new[] { "WriterID" });
+            AlterColumn("dbo.Contents", "WriterID", c => c.Int(nullable: false));
+            CreateIndex("dbo.Contents", "WriterID");
+            AddForeignKey("dbo.Contents", "WriterID", "dbo.Writers", "WriterID", cascadeDelete: true);
+        }
+    }
+}
